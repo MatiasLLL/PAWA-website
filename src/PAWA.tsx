cf64f059fe3f66799898bbb49b32850c73ddb885
MatiasLLL <matias.larralde97@gmail.com>
2024-03-27 07:53:06 -0700

    remove dead link

--- src/PAWA.tsx
+++ src/PAWA.tsx
@@ -1,11 +1,10 @@
-import logoUniswap from './assets/uni-logo.svg';
+// import logoUniswap from './assets/uni-logo.svg';
 // import BG from './assets/woooow-bg3.png';
 import { FaXTwitter } from "react-icons/fa6";
 import { LiaTelegramPlane } from "react-icons/lia";
 
 
 function Pawa() {
-  const UniswapLink = "#"
 
   return (
     <main className="bg-[url('./assets/woooow-bg3.png')] w-screen aspect-bg min-h-lvh bg-cover bg-center  font-medium bg-no-repeat text-prpl font-satoshi flex flex-col justify-around items-center lg:px-[12.75rem] md:px-10 xs:px-3 lg:text-2xl md:text-xl sm:text-md">
@@ -13,10 +12,10 @@ function Pawa() {
         <h1 className="md:text-8xl text-6xl">PAWA</h1>
       </header>
       <section className="flex justify-between mb-8 md:w-3/5 mt-7 sm:w-full">
-        <a className="flex items-center cursor-pointer pr-7 pb-10" aria-label="Buy on Uniswap" href={UniswapLink}>
+        {/* <a className="flex items-center cursor-pointer pr-7 pb-10" aria-label="Buy on Uniswap" href="">
           <span className="underline underline-offset-3">Buy on Uniswap</span>
           <img src={logoUniswap} alt="Uniswap logo" className="token-svg group w-10 h-full pl-1 pb-1 m-2" />
-        </a>
+        </a> */}
         <a className="underline underline-offset-3 cursor-pointer  pt-4 ml-2" href="https://polygonscan.com/address/0xbD2C3d9498F73988fff6af4778f1E7c8033A771C">
           View on PolygonScan
         </a>
